d0858a7d9442ccd3f85261e2b16f640248db770b
Cuong Dong <cuong.dong@gmail.com>
2015-12-24 14:48:02 -0800

    Add model and button to get new card

--- static/anki.js
+++ static/anki.js
@@ -1,6 +1,25 @@
+var cards = (function(){
+    var cards, easy, hard;
+    return {
+        initCards : function(data){
+            for (var i = 0; i < data.length; i++){
+                data["num"] = i;
+            }
+            cards = data;
+        },
+        chooseRandom : function(){
+            var randInt = Math.floor((Math.random() * cards.length));
+            return cards[randInt];
+        }
+    };
+})();
+
 var ReviewBox = React.createClass({
     getInitialState: function() {
-        return {data: []};
+        return {"currentCard": {}};
+    },
+    getNewCard: function(){
+        this.setState({"currentCard" : cards.chooseRandom()});
     },
     componentDidMount: function() {
         $.ajax({
@@ -8,7 +27,7 @@ var ReviewBox = React.createClass({
             dataType: 'json',
             cache: false,
             success: function(data) {
-                this.setState({data: data});
+                cards.initCards(data);
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -16,19 +35,21 @@ var ReviewBox = React.createClass({
         });
     },
     render: function () {
-        var test = this.state.data.map(function(card) {
+        var cardDisplay = (function(card) {
             return (
-
-                <div><FrontSide front={card.front}/>
-                <BackSide front={card.front} back={card.back}/></div>
+                <div>
+                    <FrontSide front={card.front}/>
+                    <BackSide front={card.front} back={card.back}/>
+                </div>
             );
         });
+        var card = cardDisplay(this.state.currentCard);
 
         return (
             <div className="reviewBox">
                 Review box
-                {test}
-                <Buttons/>
+                {card}
+                <Buttons handleCard={this.getNewCard}/>
             </div>
         );
     }
@@ -58,6 +79,7 @@ var Buttons = React.createClass({
     render: function () {
         return (
             <div className="buttons">
+                <button onClick={this.props.handleCard}>New card</button>
             </div>
         );
     }
