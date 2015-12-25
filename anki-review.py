6bb193e50ddc3304efd929c193e69bb001782928
Cuong Dong <cuong.dong@gmail.com>
2015-12-24 22:16:54 -0800

    Script to export Anki notes in a deck to data.js file

--- anki-review.py
+++ anki-review.py
@@ -1,18 +1,28 @@
 import anki
-from anki.utils import splitFields
+from anki.utils import ids2str, splitFields
+import json
+import os
 
 col = anki.Collection("/Users/cdong/Dropbox/cdong-ltm1/Anki/cdong/collection.anki2")
 
 data = []
 
-for id, flds, tags in col.db.execute("select guid, flds, tags from notes"):
-    row = []
+ids = col.findCards('"deck:Chinese characters"')
+
+for id, flds in col.db.execute("""
+select guid, flds from notes
+where id in
+(select nid from cards
+where cards.id in %s)""" % ids2str(ids)):
+    item = {}
 
     # fields
-    row.extend([f for f in splitFields(flds)])
+    fields = splitFields(flds)
+    item["front"] = fields[0]
+    item["back"] = fields[1]
 
-    data.append("\t".join(row))
+    data.append(item)
 
-out = "\n".join(data)
-
-print out
+file = open(os.path.dirname(os.path.realpath(__file__)) + "/static/data.js", "w")
+json.dump(data, file)
+file.close()
