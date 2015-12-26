9746c2af47b022c9b3508c192128e63eb1648c67
Cuong Dong <cuong.dong@gmail.com>
2015-12-26 08:30:51 -0800

    Get data location from URL query param instead of hard-coded value.

--- static/anki.js
+++ static/anki.js
@@ -224,7 +224,20 @@ var Buttons = React.createClass({
     }
 });
 
+var getDataParam = function(){
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === "data") { //looking for "data" key
+            return pair[1];
+        }
+    }
+
+    return "demo.js";//default data location
+};
+
 ReactDOM.render(
-    <ReviewBox url="data.js"/>,
+    <ReviewBox url={getDataParam()}/>,
     document.getElementById('content')
 );
