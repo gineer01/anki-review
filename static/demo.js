8152bbfa9ca817b68ef508dc6a72831c9626a2c5
Cuong Dong <linear.pivot@gmail.com>
2018-05-02 01:06:13 -0700

    Update demo data

--- static/demo.js
+++ static/demo.js
@@ -1,6 +1,6 @@
 [
-    {"id": "1", "front" : "主任", "back" : "director"},
-    {"id": "2", "front" : "公布", "back" : "to announce, to make public, to publish"},
-    {"id": "3", "front" : "完毕", "back" : "to finish, to end, to complete"},
-    {"id": "4", "front" : "建筑师", "back" : "architect"}
+    {"id": "1", "front" : "uno", "back" : "one"},
+    {"id": "2", "front" : "dos", "back" : "two"},
+    {"id": "3", "front" : "tres", "back" : "three"},
+    {"id": "4", "front" : "cuatro", "back" : "four"}
 ]
